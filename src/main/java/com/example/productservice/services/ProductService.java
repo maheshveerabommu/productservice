d562f33848cfd30f4ee8330ba4d2a89d601c713f
mahesh <maheshveerabommu12@gmail.com>
2024-11-09 13:12:57 +0530

    Send Response Message with status code for product not found

--- src/main/java/com/example/productservice/services/ProductService.java
+++ src/main/java/com/example/productservice/services/ProductService.java
@@ -2,12 +2,13 @@ package com.example.productservice.services;
 
 import com.example.productservice.models.Product;
 
+import javax.management.InstanceNotFoundException;
 import java.util.List;
 
 
 public interface ProductService {
 
-    public Product getProductByID(Long id);
+    public Product getProductByID(Long id) throws InstanceNotFoundException;
     public List<Product> getAllProducts();
     public Product replaceProduct(Long id, Product product);
 }
